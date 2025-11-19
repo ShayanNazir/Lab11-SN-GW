bed6786db3baab026d9629ad8d896de8625c4ea9
ShayanNazir <shayan.nazir@ufl.edu>
2025-11-19 14:45:01 -0500

    Implement basic calculator functions

--- calculator.py
+++ calculator.py
@@ -1,11 +1,37 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero (a = 0).")
+    return b / a
+
+
+def logarithm(a, b):
+
+    if a <= 0 or a == 1:
+        raise ValueError("Log base must be positive and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Log argument must be positive.")
+    return math.log(b, a)
+
+
+def exponent(a, b):
+
+    return a ** b
+
 
 
